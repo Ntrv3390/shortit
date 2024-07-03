3f5b97c9a55d8f359074db60591d0ca546f2d8b6
Ntrv3390 <78497133+Ntrv3390@users.noreply.github.com>
2024-07-03 18:07:52 +0530

    fixes

--- src/components/Url.jsx
+++ src/components/Url.jsx
@@ -4,7 +4,7 @@ const Url = (props) => {
   return (
     <tr className="bg-[#F4EEE0] border-b text-[#393646]">
       <th scope="row" className="px-6 py-4 font-medium whitespace-nowrap d">
-        {process.env.LOCAL_BASE_URL + '/' + props.shortId}
+        {'https://'+process.env.LOCAL_BASE_URL + '/' + props.shortId}
       </th>
       <td className="px-6 py-4">{props.redirectUrl}</td>
       <td className="px-6 py-4"><Link href={`/url/${props.id}`}>Details</Link></td>
